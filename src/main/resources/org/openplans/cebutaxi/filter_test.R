f35bd491f35215cc930b3cb77e276a65c14e1844
Brandon Willard <brandonwillard@gmail.com>
2012-04-08 20:44:32 -0500

    - fixed to R script

--- src/main/resources/org/openplans/cebutaxi/filter_test.R
+++ src/main/resources/org/openplans/cebutaxi/filter_test.R
@@ -1,16 +1,15 @@
 library(Matrix)
 library(dlm)
-library(ggplot2)
 library(lattice)
 library(latticeExtra)
 
 proj_data = ts(read.csv("test_data/proj_output.csv")[,2:3])
 colnames(proj_data)
 
-timeDiff = 30
-turnRate = 0.01
-aVariance = 5
-gVariance = 10
+# in minutes
+timeDiff = 1
+aVariance = 25
+gVariance = 50
 
 m0 = c(proj_data[1, 1], 0, proj_data[1,2], 0)
 Ct = diag(rep(1,4)) * aVariance
@@ -18,10 +17,9 @@ Ct = diag(rep(1,4)) * aVariance
 # Classic kinematics
 F = diag(c(1,1))
 F[1,2] = timeDiff
-Ft = matrix(rep(0, 16), nrow=4, ncol=4)
-Ft[1:2, 1:2] = F
-Ft[3:4, 3:4] = F
+Ft = diag(2) %x% F
 # Constant Turn-rate model
+#turnRate = 0.01
 #Ft = cbind(c(1,0,0,0), 
 #           c(timeDiff, 1-(turnRate*timeDiff)^2/2, 
 #             turnRate*timeDiff^2/2, turnRate*timeDiff), 
@@ -30,14 +28,12 @@ Ft[3:4, 3:4] = F
 #             timeDiff, 1-(turnRate*timeDiff)^2/2))
 
 G = c(timeDiff^2/2, timeDiff)
-GG = matrix(rep(0, 4), nrow=4, ncol=2)
-GG[1:2, 1] = G
-GG[3:4, 2] = G
-QQtmp = drop(GG %*% diag(rep(1,2)*aVariance)) %*% t(GG)
+GG = diag(2) %x% G
+QQtmp = drop(GG %*% diag(c(aVariance,aVariance))) %*% t(GG)
 QQ = as.matrix(nearPD(QQtmp)$mat)
 #eigen(QQ, symmetric=T, only.values=T)
 
-O = cbind(diag(c(1,0)), c(0,1), c(0,0))
+O = t(diag(2) %x% c(1,0))
 
 ObsM = rbind(O, O%*%Ft)
 sum(svd(ObsM)$d > 0)
@@ -46,15 +42,15 @@ filter = dlm(m0 = m0, C0 = Ct,
              FF = O, V = gVariance*diag(c(1,1)),
              GG = Ft, W = QQ)
 
-data_window = window(proj_data, end=100)
+data_window = window(proj_data, end=80)
 res = dlmFilter(data_window, filter)
 str(res, max.level=1)
 str(res$f, max.level=1)
 
 
 sdev = residuals(res)$sd
-lwr = res$f + qnorm(0.25)*sdev
-upr = res$f - qnorm(0.25)*sdev
+lwr = res$f + qnorm(0.025)*sdev
+upr = res$f - qnorm(0.025)*sdev
 p1 = xyplot(cbind(data_window,res$f), type='n', 
             screens=c(1,2,1,2))
 p1 = p1 + xyplot(data_window[,1], type='l', col="black", screens=1)
@@ -67,11 +63,5 @@ p1 = p1 + xyplot(upr[,1], type='l', col="red", lty=2, screens=1)
 p1 = p1 + xyplot(lwr[,2], type='l', col="red", lty=2, screens=2)
 p1 = p1 + xyplot(res$f[,2], type='l', col="red", screens=2)
 p1 = p1 + xyplot(upr[,2], type='l', col="red", lty=2, screens=2)
-#p1 = p1 + layer(panel.xyplot(1:length(res$f), res$f, 
-#                             type='l', 
-#                             col="red", lwd=1, lty=1), rows=1)
-#p1 = p1 + layer(panel.xyplot(1:length(res$f), res$f, 
-#                             type='l', 
-#                             col="red", lwd=1, lty=1), rows=2)
 plot(p1)
 
