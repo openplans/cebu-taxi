d76cf3d964142ce7497ed6c7bf867f912a5ab8bb
brandon willard <brandonwillard@gmail.com>
2012-04-09 11:37:54 -0500

    - additions to the R script

--- src/main/resources/org/openplans/cebutaxi/filter_test.R
+++ src/main/resources/org/openplans/cebutaxi/filter_test.R
@@ -7,7 +7,7 @@ proj_data = ts(read.csv("test_data/proj_output.csv")[,2:3])
 colnames(proj_data)
 
 # in minutes
-timeDiff = 1
+timeDiff = 1                                                          
 aVariance = 25
 gVariance = 50
 
@@ -30,8 +30,11 @@ Ft = diag(2) %x% F
 G = c(timeDiff^2/2, timeDiff)
 GG = diag(2) %x% G
 QQtmp = drop(GG %*% diag(c(aVariance,aVariance))) %*% t(GG)
-QQ = as.matrix(nearPD(QQtmp)$mat)
-#eigen(QQ, symmetric=T, only.values=T)
+if (all(eigen(QQtmp, symmetric=T, only.values=T)$values >= 0)) {
+  QQ = QQtmp
+} else {
+  QQ = as.matrix(nearPD(QQtmp)$mat)
+}
 
 O = t(diag(2) %x% c(1,0))
 
@@ -42,7 +45,7 @@ filter = dlm(m0 = m0, C0 = Ct,
              FF = O, V = gVariance*diag(c(1,1)),
              GG = Ft, W = QQ)
 
-data_window = window(proj_data, end=80)
+data_window = window(proj_data, end=400)
 res = dlmFilter(data_window, filter)
 str(res, max.level=1)
 str(res$f, max.level=1)
@@ -51,8 +54,8 @@ str(res$f, max.level=1)
 sdev = residuals(res)$sd
 lwr = res$f + qnorm(0.025)*sdev
 upr = res$f - qnorm(0.025)*sdev
-p1 = xyplot(cbind(data_window,res$f), type='n', 
-            screens=c(1,2,1,2))
+p1 = xyplot(cbind(data_window,res$f, lwr, upr), type='n', 
+            screens=c(1,2,1,2,1,2,1,2))
 p1 = p1 + xyplot(data_window[,1], type='l', col="black", screens=1)
 p1 = p1 + xyplot(data_window[,2], type='l', col="black", screens=2)
 
