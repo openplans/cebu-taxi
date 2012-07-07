72ad6859911ce8c0ad550013fd5d989e65749bc0
Kevin Webb <kpwebb@gmail.com>
2012-07-07 15:14:47 -0400

    adding native blas support

--- cebu-server/app/jobs/Bootstrap.java
+++ cebu-server/app/jobs/Bootstrap.java
@@ -7,6 +7,13 @@ import models.*;
 @OnApplicationStart
 public class Bootstrap extends Job {
     public void doJob() {
+    	
+    	Logger.info("loading native BLAS libs");
+    	
+    	Play.getFile("lib/libjniarpack-linux-x86_64.so");
+    	Play.getFile("lib/libjniblas-linux-x86_64.so");
+    	Play.getFile("lib/libjnilapack-linux-x86_64.so");
+    	
         // Check if the database is empty
         if(Operator.count() == 0) {
             Fixtures.loadModels("initial-data.yml");
