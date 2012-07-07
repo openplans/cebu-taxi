d9d54279f55078f5618d60b71688b1c950f63837
Kevin Webb <kpwebb@gmail.com>
2012-07-07 03:01:48 -0400

    added map view

---
+++ cebu-server/public/javascripts/cebu.js
@@ -0,0 +1,28 @@
+
+var startLatLng = new L.LatLng(10.3181373, 123.8956844); 
+
+var mbUrl = 'http://{s}.tiles.mapbox.com/v3/openplans.map-g4j0dszr/{z}/{x}/{y}.png';
+
+var cebuUrl = 'http://cebutraffic.org/tiles/{z}/{x}/{y}.png';
+
+var mbAttrib = 'Map data &copy; 2011 OpenStreetMap contributors, Imagery &copy; 2011 CloudMade';
+var mbOptions = {
+  maxZoom : 17,
+  attribution : mbAttrib
+};
+
+
+$(document).ready(function() {
+
+  map = new L.Map('map');
+
+  var mb = new L.TileLayer(mbUrl, mbOptions);
+  map.addLayer(mb);
+
+  var cebu = new L.TileLayer(cebuUrl, mbOptions);
+  map.addLayer(cebu);
+
+  map.setView(startLatLng, 15, true);
+
+
+});
